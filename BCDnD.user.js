e03838eceb95e6c5e1191354f66053760e0dff47
Apathy23 <robernesje@yahoo.no>
2024-07-20 04:30:35 +0200

    test

---
+++ BCDnD.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name BCDND
+// @namespace https://www.bondageprojects.com/
+// @version 1.0
+// @description 
+// @author 
+// @match https://bondageprojects.elementfx.com/*
+// @match https://www.bondageprojects.elementfx.com/*
+// @match https://bondage-europe.com/*
+// @match https://www.bondage-europe.com/*
+// @match http://localhost:*/*
+// @icon 
+// @grant none
+// @run-at document-end
+// ==/UserScript==
+
+
+import(
+	`https://apathy23.gitlab.io/bc-addon-loader/index.js?v=${(
+		Date.now() / 10000
+	).toFixed(0)}`
+)
