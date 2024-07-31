fb4d08f969f07642bd85789f0f7acbe3569e284d
Apathy23 <robernesje@yahoo.no>
2024-07-31 04:45:19 +0200

    test

--- bcdnd.js
+++ bcdnd.js
@@ -116,14 +116,15 @@ async function runBCDnD() {
     }
 
     function checkTrap() {
-        if (!Player.MapData) return;
-        for (let i = 0; i < trapArray.length; i++) {
-            for (let j = 0; j < ChatRoomCharacter.length; j++) {
-                if (InventoryGet(ChatRoomCharacter[j], trapArray[i].slot) == null && trapArray[i].active == true) {
-                    if (ChatRoomCharacter[j].MapData.Pos.X == trapArray[i].position.X && ChatRoomCharacter[j].MapData.Pos.Y == trapArray[i].position.Y) {
-                        InventoryWear(ChatRoomCharacter[j], trapArray[i].name, trapArray[i].slot, trapArray[i].color, 5, ChatRoomCharacter[j].ID, null, true);
-                        ServerSend("ChatRoomChat", { Content: trapArray[i].dialog, Type: "Emote", Target: ChatRoomCharacter[j].MemberNumber });
-                        ChatRoomCharacterUpdate(ChatRoomCharacter[j]);
+        if (Player.MapData) {
+            for (let i = 0; i < trapArray.length; i++) {
+                for (let j = 0; j < ChatRoomCharacter.length; j++) {
+                    if (InventoryGet(ChatRoomCharacter[j], trapArray[i].slot) == null && trapArray[i].active == true) {
+                        if (ChatRoomCharacter[j].MapData.Pos.X == trapArray[i].position.X && ChatRoomCharacter[j].MapData.Pos.Y == trapArray[i].position.Y) {
+                            InventoryWear(ChatRoomCharacter[j], trapArray[i].name, trapArray[i].slot, trapArray[i].color, 5, ChatRoomCharacter[j].ID, null, true);
+                            ServerSend("ChatRoomChat", { Content: trapArray[i].dialog, Type: "Emote", Target: ChatRoomCharacter[j].MemberNumber });
+                            ChatRoomCharacterUpdate(ChatRoomCharacter[j]);
+                        }
                     }
                 }
             }
